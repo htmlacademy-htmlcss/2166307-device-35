2be460de71a374c8b5338fbeb5c0e31d28f03c56
AndrewPlatonov888 <very@blacky.ru>
2022-12-13 01:13:09 +0300

    открытие модалки через JS

--- scripts/modal-master.js
+++ scripts/modal-master.js
@@ -1,24 +1,25 @@
+function showModalWin() {
+
+  var darkLayer = document.createElement('div'); // слой затемнения
+  darkLayer.id = 'shadow'; // id чтобы подхватить стиль
+  document.body.appendChild(darkLayer); // включаем затемнение
+
+  var modalWin = document.getElementById('popupWin'); // находим наше окошко
+  modalWin.style.display = 'block'; // "включаем" его
 
 
-$(function() {
+  darkLayer.onclick = function () {  // при клике на слой затемнения все исчезнет
+    darkLayer.parentNode.removeChild(darkLayer); // удаляем затемнение
+    modalWin.style.display = 'none'; // выключаем модальное окно
+    return false;
+  };
+}
 
-    (function quantityProducts() {
-      var $quantityArrowMinus = $(".quantity-arrow-minus");
-      var $quantityArrowPlus = $(".quantity-arrow-plus");
-      var $quantityNum = $(".quantity-num");
-
-      $quantityArrowMinus.click(quantityMinus);
-      $quantityArrowPlus.click(quantityPlus);
-
-      function quantityMinus() {
-        if ($quantityNum.val() > 1) {
-          $quantityNum.val(+$quantityNum.val() - 1);
-        }
-      }
-
-      function quantityPlus() {
-        $quantityNum.val(+$quantityNum.val() + 1);
-      }
-    })();
-
-  });
+function handleClick() {  // обработчик кнопки закрытия формы
+  var modalWin = document.getElementById('popupWin'); // находим наше окошко
+  var darkLayer = document.getElementById('shadow'); // находим слой затемнения
+  darkLayer.parentNode.removeChild(darkLayer); // выключаем затемнение
+  document.getElementById('modal-delivery-window').reset() // делаем сброс формы перед выключением
+  modalWin.style.display = 'none'; // выключаем модальное окно
+  return false;
+};
