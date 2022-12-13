f4c1ba5aef01e82aaeb85119e3aa94b54b400893
AndrewPlatonov888 <very@blacky.ru>
2022-12-13 12:40:43 +0300

    input number в модалке через JS

--- scripts/modal-master.js
+++ scripts/modal-master.js
@@ -1,12 +1,13 @@
+//-----------↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓------- ОТКРЫТИЕ-ЗАКРЫТИЕ МОДАЛЬНОГО ОКНА ---------------------------------------------
+
 function showModalWin() {
 
   var darkLayer = document.createElement('div'); // слой затемнения
-  darkLayer.id = 'shadow'; // id чтобы подхватить стиль
+  darkLayer.className = "shadow" // даем новому элементу класс, чтобы подхватить стиль
   document.body.appendChild(darkLayer); // включаем затемнение
 
   var modalWin = document.getElementById('popupWin'); // находим наше окошко
-  modalWin.style.display = 'block'; // "включаем" его
-
+  modalWin.style.display = 'block'; // делаем его видимым
 
   darkLayer.onclick = function () {  // при клике на слой затемнения все исчезнет
     darkLayer.parentNode.removeChild(darkLayer); // удаляем затемнение
@@ -15,11 +16,28 @@ function showModalWin() {
   };
 }
 
-function handleClick() {  // обработчик кнопки закрытия формы
+function closeClick() {  // обработчик кнопки закрытия модального окна
   var modalWin = document.getElementById('popupWin'); // находим наше окошко
-  var darkLayer = document.getElementById('shadow'); // находим слой затемнения
+  var darkLayer = document.querySelectorAll('.shadow')[0]; // находим слой затемнения
   darkLayer.parentNode.removeChild(darkLayer); // выключаем затемнение
-  document.getElementById('modal-delivery-window').reset() // делаем сброс формы перед выключением
+  document.getElementById('modal-delivery-window').reset() // делаем сброс формы перед выключением модального окна
   modalWin.style.display = 'none'; // выключаем модальное окно
   return false;
 };
+
+//----------↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑------- ОТКРЫТИЕ-ЗАКРЫТИЕ МОДАЛЬНОГО ОКНА ---------------------------------------------
+
+
+//-----------↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓------ ОБРАБОТЧИК для INPUT type=NUMBER ---------------------------------------------
+
+function minusClick() {
+  document.querySelectorAll('.quantity-arrow-minus')[0].nextElementSibling.stepDown();  // используем JS метод stepDown для уменьшения значения
+  document.querySelectorAll('.quantity-arrow-minus')[0].nextElementSibling.onchange();
+};
+
+function plusClick() {
+  document.querySelectorAll('.quantity-arrow-plus')[0].previousElementSibling.stepUp(); // используем JS метод stepUP для увеличения значения
+  document.querySelectorAll('.quantity-arrow-plus')[0].previousElementSibling.onchange();
+};
+
+//----------↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑ ↑------- ОБРАБОТЧИК для INPUT type=NUMBER ---------------------------------------------
